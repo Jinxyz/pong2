04e2924301a8104347ece1267fee218f86fb7da9
rasmus.gillberg <xxyaymanyayxx@hotmail.com>
2021-05-04 13:38:36 +0200

    Arbetar med connect-feature

--- src/Client.java
+++ src/Client.java
@@ -1,2 +1,27 @@
+import javax.swing.*;
+import java.net.Socket;
+
+import static java.lang.System.out;
+
 public class Client {
+    Socket socket = null;
+
+    public Client() {
+        String ip = (String) JOptionPane.showInputDialog(null, "IP?", "Connect to..", JOptionPane.QUESTION_MESSAGE);
+        int port = Integer.parseInt(JOptionPane.showInputDialog(null, "Port?", "Connect to..", JOptionPane.QUESTION_MESSAGE));
+
+        // try socket, if socket false close program
+        try {
+            socket = new Socket(ip, port);
+        } catch (Exception e) {
+            System.out.println("Client failed to connect");
+            System.exit(0);
+        }
+    }
+
+    public void close() throws Exception{
+        out.close();
+        socket.close();
+        System.out.println("Done!");
+    }
 }
--- src/Server.java
+++ src/Server.java
@@ -1,2 +1,34 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class Server {
+    public static void main(String[] args) throws IOException {
+        int port = 1234;
+        boolean run = true;
+        ServerSocket serverSocket;
+        Socket socket;
+
+        System.out.println("Server started.");
+        serverSocket = new ServerSocket(port);
+
+        while (true) {
+            System.out.println("Waiting for connections!");
+            socket = serverSocket.accept();
+
+            // Go
+            PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+            BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            System.out.println("Client connected!");
+
+            // close server
+            in.close();
+            out.close();
+            socket.close();
+            // System.out.println("Closing down " + name);
+        }
+    }
 }
